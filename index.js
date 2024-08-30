9222edde376907adf3392bc8ccdca9b6073acf12
S-Rashy <sadiqrashidah@gmail.com>
2024-08-30 04:23:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// const ola = "fish"
+// console.log(ola);
+
+const water = [
+    {name: "ola", class: 2},
+    {name: "olu", class: 3},
+    {name: "oma", class: 4},
+    {name: "ota", class: 2}
+]
+console.log(typeof water);
+// const newp = console.log(water.splice(1,1));
+// console.log(newp);
+water.splice(2,0,"baby")
+console.log(water);
+water[1].splice
+
+
+
